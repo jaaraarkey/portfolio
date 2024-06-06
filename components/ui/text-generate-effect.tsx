2cfb08c7b220507b17045275c1f042d3f2732de6
jaaraarkey <olegudyachenko@gmail.com>
2024-06-06 19:22:06 +0200

    first commit

---
+++ components/ui/text-generate-effect.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/utils/cn";
+
+export const TextGenerateEffect = ({
+    words,
+    className,
+}: {
+    words: string;
+    className?: string;
+}) => {
+    const [scope, animate] = useAnimate();
+
+    let wordsArray = words.split(" ");
+
+    useEffect(() => {
+        // Animate the spans when the scope changes
+        animate(
+            "span",
+            {
+                opacity: 1,
+            },
+            {
+                duration: 2,
+                delay: stagger(0.2),
+            }
+        );
+    }, [scope.current]);
+
+    const renderWords = () => {
+        return (
+            <motion.div ref={scope}>
+                {wordsArray.map((word, idx) => {
+                    return (
+                        <motion.span
+                            key={word + idx}
+                            // In order to change accent color and the accented word change the line below:
+                            className={`${idx > 1 ? 'text-purple' : 'dark:text-white text-black '} opacity-0`}
+                        >
+                            {word}{" "}
+                        </motion.span>
+                    );
+                })}
+            </motion.div>
+        );
+    };
+
+    return (
+        <div className={cn("font-bold", className)}>
+            <div className="mt-4">
+                <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+                    {renderWords()}
+                </div>
+            </div>
+        </div>
+    );
+};
